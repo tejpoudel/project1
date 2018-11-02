519439315117464310d05a6d1d1540fbba6ad54f
Rohit Chhetri <rohit.chhetri@gmail.com>
2018-11-01 18:25:55 -0700

    youtube Embeded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
-alert("Ready to work");
+//alert("Ready to work");
 
 //TODO Movie OMDb API
 // TODO Youtube API Working 
 
 
-//Ceh
+//Api Information -
+//Youtube KEY = AIzaSyCb3e79c57_B7L6m214mksu6V3b_k_xEtA
+//OMDb Key - 388edf5a
 
 function movieinfo() {
         
-        var queryURL = "http://www.omdbapi.com/?t=rock&apikey=//!InsertKey";
+        var queryURL = "http://www.omdbapi.com/?t=rock&apikey=388edf5a";
         // Creating an AJAX call for the specific movie 
         $.ajax({
           url: queryURL,
@@ -21,7 +23,7 @@ movieinfo();
 
 function youtube(){
 
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=The+Rock+Trailer&type=video&key=//!InsertKey";
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=The+Rock+Trailer&type=video&key=AIzaSyCb3e79c57_B7L6m214mksu6V3b_k_xEtA";
     //Creating an Ajax call for the specific trailer 
 
     $.ajax({
